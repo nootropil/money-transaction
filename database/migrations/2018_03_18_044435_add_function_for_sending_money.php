15b4a9b7814d8998151a86f8964eafe274926cf6
sasah <alexandrpopov184@yandex.ru>
2018-03-18 17:48:02 +0700

    Added coments for stored function

--- database/migrations/2018_03_18_044435_add_function_for_sending_money.php
+++ database/migrations/2018_03_18_044435_add_function_for_sending_money.php
@@ -15,23 +15,28 @@ class AddFunctionForSendingMoney extends Migration
         DB::unprepared("
         CREATE FUNCTION send_money_from_user_to_user (usernameFrom TEXT, usernameTo TEXT, amountOfMoney INT) RETURNS void AS $$         
             BEGIN
-            
+                
+                -- Блокируем доступ всех остальных транзакций до таблицы user
                 LOCK TABLE public.user IN ACCESS EXCLUSIVE MODE;  
                                 
-                IF ((SELECT EXISTS(SELECT 1 FROM public.user where username = usernameFrom)) IS FALSE) THEN
+                -- Проверяем существование пользователя usernameFrom
+                IF ((SELECT EXISTS(SELECT 1 FROM public.user where username = usernameFrom)) IS FALSE) THEN 
                     RAISE EXCEPTION '||Пользователь % не существует||', usernameFrom;
-                END IF;
-                    
-                IF ((SELECT EXISTS(SELECT 1 FROM public.user where username = usernameTo)) IS FALSE) THEN
+                END IF;                
+                
+                -- Проверяем существование пользователя usernameTo    
+                IF ((SELECT EXISTS(SELECT 1 FROM public.user where username = usernameTo)) IS FALSE) THEN 
                     RAISE EXCEPTION '||Пользователь % не существует||', usernameTo;
                 END IF;
-                    
+                
+                -- Проверяем что пользователь usernameFrom имеет достаточно денег на счету  
                 IF ((SELECT EXISTS(SELECT 1 FROM public.user where username = usernameFrom AND money >= amountOfMoney)) IS FALSE) THEN
                     RAISE EXCEPTION '||Не достаточно средств на счету пользователя %||', usernameFrom;
                 END IF;
-                    
-                UPDATE public.user SET money = money - amountOfMoney WHERE username = usernameFrom;
-                UPDATE public.user SET money = money + amountOfMoney WHERE username = usernameTo; 
+                
+                -- Списываем деньги со счёта usernameFrom и зачисляем на счёт usernameTo  
+                UPDATE public.user SET money = money - amountOfMoney WHERE username = usernameFrom; 
+                UPDATE public.user SET money = money + amountOfMoney WHERE username = usernameTo;
                  
             END;            
         $$ LANGUAGE plpgsql;        
